0ad704fca7a580c7b9731cde6020c2c906baed15
Matt Gordon <bigmattsize21@gmail.com>
2020-05-29 08:33:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Matt"
+  end
+  
+  get 'hometown' do
+    "My hometown is Reno"
+  end
+  
+  get '/favorite '
 end
